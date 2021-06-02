5ea7ae238ed08a67fb0823306a5114adf6d98113
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-06-01 19:04:41 -0600

    added main login, added form code to login

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        return "posted"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
