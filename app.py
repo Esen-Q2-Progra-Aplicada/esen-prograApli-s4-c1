da1eaf47a281d2863dbefc86ff72c7d0b6f78dff
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-06-01 20:05:17 -0600

    added recaptcha form code and server side verification

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -10,12 +11,28 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    data = {}
     if request.method == "GET":
         company = "balcorp"
         return render_template("login.html", company=company)
     elif request.method == "POST":
-        month = request.form["month"]
-        return f"posted month:{month}"
+        data["secret"] = "6LeAdQcbAAAAAGNn732kkStupieUDdKjQTl38KL_"
+        data["response"] = request.form["g-recaptcha-response"]
+        response = requests.post(
+            "https://www.google.com/recaptcha/api/siteverify", params=data
+        )
+        print(response)
+        if response.status_code == 200:
+            messageJson = response.json()
+            print(messageJson)
+            if messageJson["success"]:
+                user = request.form["user"]
+                passwd = request.form["passwd"]
+                return f"posted {user}::{passwd}"
+            else:
+                return f"posted {messageJson['error-codes'][0]}"
+        else:
+            return f"posted error"
 
 
 if __name__ == "__main__":
